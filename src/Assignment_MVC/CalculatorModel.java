732dc3c6a1fa504bdece1eee000d4d86ba8e2604
Shady-Max <kolodin2005+github@gmail.com>
2024-10-24 16:52:29 +0500

    Implemented Calculator Model

--- src/Assignment_MVC/CalculatorModel.java
+++ src/Assignment_MVC/CalculatorModel.java
@@ -2,12 +2,23 @@ package Assignment_MVC;
 
 public class CalculatorModel {
 
-    public double add (double a, double b) { throw new UnsupportedOperationException("Not supported yet."); }
+    public double add (double a, double b) {
+        return a + b;
+    }
 
-    public double subtract (double a, double b) { throw new UnsupportedOperationException("Not supported yet."); }
+    public double subtract (double a, double b) {
+        return a - b;
+    }
 
-    public double multiply (double a, double b) { throw new UnsupportedOperationException("Not supported yet."); }
+    public double multiply (double a, double b) {
+        return a * b;
+    }
 
-    public double divide (double a, double b) { throw new UnsupportedOperationException("Not supported yet."); }
+    public double divide (double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Ошибка: деление на ноль");
+        }
+        return a / b;
+    }
 
 }
